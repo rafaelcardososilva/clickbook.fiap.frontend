aaf529fe0a670654b9abd950bd9f5322eb273ae4
Rafael Cardoso <cardosorafael@outlook.com.br>
2022-04-08 14:19:36 -0300

    feat: criando projeto

---
+++ src/pages/Feed/Feed.tsx
@@ -0,0 +1,12 @@
+import { Heading } from '@chakra-ui/react';
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Feed: React.FC = () => {
+  return (
+    <Heading>Feed</Heading>
+  );
+}
+
+export default Feed;
